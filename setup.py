4bb30c14b8b8e01889bfa1d0868ea9e50d161b9d
Jan Pipek <jan.pipek@gmail.com>
2015-05-02 21:28:51 +0200

    First reasonable commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import itertools
+
+options = dict(
+    name='gamma_index',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='gamma_index - calculation of gamma index on multi-dimensional distributions',
+    long_description=open('README.md').read(),
+    author='Jan Pipek',
+    author_email='jan.pipek@gmail.com',
+    url='https://github.com/janpipek/gamma_index',
+    install_requires = ['numpy']
+)
+setup(**options)
